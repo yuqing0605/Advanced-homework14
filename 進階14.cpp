f490deb491c1f9109b404c454c425eeac898106a
yuqing0605 <48577102+yuqing0605@users.noreply.github.com>
2019-05-31 11:24:54 +0800

    Add files via upload

---
+++ 進階14.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(void){
+
+	int length;
+	int i;
+	char input[]="I $am 5a$ ba68d#@$ st-ude[nt].";
+	char *check;
+	length=strlen(input);
+	check=input;
+	
+	for(i=0;i<length;i++){
+
+		if(*(check+i)<122&&*(check+i)>=96){
+			printf("%c", *(check+i));
+		}
+
+		else if(*(check+i)<91&&*(check+i)>64){
+			printf("%c", *(check+i));
+		}
+
+		else if(*(check+i)==32){
+			printf("%c", *(check+i));
+		}
+	}
+	printf("\n\n");
+	system("pause");
+	return 0;
+} 
